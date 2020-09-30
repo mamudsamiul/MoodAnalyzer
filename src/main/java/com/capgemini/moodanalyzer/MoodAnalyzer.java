dde9a927fb8f805a538b6cdcf9728b86a3b606c1
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-30 09:38:18 +0530

    (Samiul Mamud) Added: Null pointer exception handling is introduced

--- src/main/java/com/capgemini/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/capgemini/moodanalyzer/MoodAnalyzer.java
@@ -6,9 +6,14 @@ public class MoodAnalyzer {
 	}
 
 	public String analyseMood(String message) {
-		if (message.contains("sad"))
-			return "SAD";
-		else
+		try {
+			if (message.contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (Exception e) {
+			System.out.println("Exception thrown");
 			return "HAPPY";
+		}
 	}
 }
